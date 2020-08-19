5f417458aeb3d06a2dea6032390b848a38c118a0
Jia <jiavon@ualberta.ca>
2020-08-18 20:44:45 -0600

    added home column and address successfully refer to img folder

--- challengeassignment.sql
+++ challengeassignment.sql
@@ -3,4 +3,8 @@
 ALTER TABLE `address`
 ADD `Home` varchar(50);
 
---Add address for the customer with account number AW00000222 with 
+--Add address for the customer with account number AW00000222 with 1 Churchill Square Edmonton AB A1A 1A1 
+
+UPDATE address SET Home = '1 Churchill Square Edmonton AB A1A 1A1' WHERE AddressID = '564' 
+
+--Result is screen capped. 
